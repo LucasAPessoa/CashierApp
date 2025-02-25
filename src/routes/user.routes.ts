427aa3eec94884a7f95687d8282a71bdbd979538
Lucas <lucasapessoa@outlook.com>
2025-02-24 23:51:23 -0300

    Add user routes for create, get, and soft delete (patch) operations

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,8 @@
+import { FastifyInstance } from "fastify";
+import { UserController } from "../controllers/user.controller";
+
+export async function UserRoutes(app: FastifyInstance) {
+    app.post("/users", UserController.createUser);
+    app.get("/users", UserController.getUsers);
+    app.patch("/users/:id", UserController.deleteUser);
+}
