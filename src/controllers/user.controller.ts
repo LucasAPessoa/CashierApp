fb4c16d353cd92a1298afbd8988afcd116793826
Lucas <lucasapessoa@outlook.com>
2025-02-24 23:46:32 -0300

    add createUser method to UserController

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,33 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
 import { UserRepository } from "../repositories/user.repository";
+
 export class UserController {
     private static userRepository = new UserRepository();
+
+    static async createUser(request: FastifyRequest, reply: FastifyReply) {
+        const { name, email, password } = request.body as {
+            name: string;
+            email: string;
+            password: string;
+        };
+
+        const emailExists = await UserController.userRepository.findByEmail(
+            email
+        );
+
+        if (emailExists) {
+            return reply.code(400).send({
+                message: "Email already exists",
+            });
+        }
+
+        const user = await UserController.userRepository.createUser(
+            name,
+            email,
+            password
+        );
+
+        return reply.status(200).send(user);
+    }
 }
