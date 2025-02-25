88ab260f07ba3caca7668d4b2cfb29950514a2af
Lucas <lucasapessoa@outlook.com>
2025-02-24 23:51:45 -0300

    register routes in fastify

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import fastify from "fastify";
 
+import { UserRoutes } from "./routes/user.routes";
+
 const app = fastify();
+
+app.register(UserRoutes);
 
 app.listen({ port: 3000, host: "localhost" }, () => {
     console.log("Server is running on http://localhost:3000");
