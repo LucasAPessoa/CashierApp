f01b42d741221e46d1f012093d6bd74b3e617eba
Lucas <lucasapessoa@outlook.com>
2025-02-26 10:58:10 -0300

    refactor UserRoutes to improve user management endpoints

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,11 +2,24 @@ import { FastifyInstance } from "fastify";
 import { UserController } from "../controllers/user.controller";
 
 export async function UserRoutes(app: FastifyInstance) {
+    //Cria um novo usuário
     app.post("/users", UserController.createUser);
-    app.get("/users", UserController.getUsers);
+
+    //Lista usuários ativos
+    app.get("/users", UserController.getActiveUsers);
+
+    //Deleta um usuário
     app.patch("/users/:id", UserController.deleteUser);
-    app.get("/users/:id", UserController.getUserById);
+
+    //Retorna um usuário ativo pelo Id
+    app.get("/users/:id", UserController.getActiveUserById);
+
+    //Retorna um usuário deletado pelo Id
     app.get("/deletedUsers/:id", UserController.getDeletedUserById);
+
+    //Atualiza name, password e email de um usuário
     app.put("/users/:id", UserController.updateUser);
+
+    //Retorna todos os usuários, deltados ou ativos
     app.get("/usersAll", UserController.getAllUsers);
 }
