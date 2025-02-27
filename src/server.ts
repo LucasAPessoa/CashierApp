25490731277a1c8d853e12c315e0f524057e028f
Lucas <lucasapessoa@outlook.com>
2025-02-27 18:14:11 -0300

    register CategoryRoutes in server configuration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
 
 import { UserRoutes } from "./routes/user.routes";
+import { CategoryRoutes } from "./routes/category.routes";
 
 const app = fastify();
 
+app.register(CategoryRoutes);
 app.register(UserRoutes);
 
 app.listen({ port: 3000, host: "localhost" }, () => {
