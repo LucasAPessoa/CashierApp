1539059cd76aaf1b75f56f59119701144ce51c62
Lucas <lucasapessoa@outlook.com>
2025-02-27 18:12:08 -0300

    add createCategory method to CategoryController for category creation

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -3,4 +3,28 @@ import { CategoriyRepository } from "../repositories/category.repository";
 import { CategoryType } from "@prisma/client";
 export class CategoryController {
     private static categoriyRepository = new CategoriyRepository();
+
+    static async createCategory(request: FastifyRequest, reply: FastifyReply) {
+        const { name, type } = request.body as {
+            name: string;
+            type: CategoryType;
+        };
+
+        const categoryExists =
+            await CategoryController.categoriyRepository.findCategoryByName(
+                name
+            );
+
+        if (categoryExists) {
+            return reply.code(400).send({ message: "Category already exists" });
+        }
+
+        const category =
+            await CategoryController.categoriyRepository.createCategory(
+                name,
+                type
+            );
+
+        return reply.status(200).send(category);
+    }
 }
