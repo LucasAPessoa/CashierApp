64993f729d923ce48b1dae22204246057cd2b25f
Lucas <lucasapessoa@outlook.com>
2025-02-27 18:11:31 -0300

    add CategoryController class to category controller

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,3 +1,5 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { CategoriyRepository } from "../repositories/category.repository";
 import { CategoryType } from "@prisma/client";
+export class CategoryController {
+}
