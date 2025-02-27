eaf862c6d4479c6a6c4d16f106e067608190f4ea
Lucas <lucasapessoa@outlook.com>
2025-02-27 18:08:33 -0300

    add findActiveCategoryById method to CategoryRepository

--- src/repositories/category.repository.ts
+++ src/repositories/category.repository.ts
@@ -2,4 +2,12 @@ import { CategoryType } from "@prisma/client";
 import { prisma } from "../lib/prisma";
 
 export class CategoriyRepository {
+    async findActiveCategoryById(id: number) {
+        return prisma.categories.findUnique({
+            where: {
+                id: id,
+                deletedAt: null,
+            },
+        });
+    }
 }
