29746e288862fb4d1d6357cba48d1b9f154923b5
Lucas <lucasapessoa@outlook.com>
2025-02-27 18:14:48 -0300

    add route for retrieving all categories

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -3,4 +3,5 @@ import { CategoryController } from "../controllers/category.controller";
 
 export async function CategoryRoutes(app: FastifyInstance) {
     app.post("/categories", CategoryController.createCategory);
+    app.get("/categories", CategoryController.getAllCategories);
 }
