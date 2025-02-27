710d5e20fedf2577f04950f4591a72af528920e4
Lucas <lucasapessoa@outlook.com>
2025-02-27 18:14:38 -0300

    add route for creating a new category

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -2,4 +2,5 @@ import { FastifyInstance } from "fastify";
 import { CategoryController } from "../controllers/category.controller";
 
 export async function CategoryRoutes(app: FastifyInstance) {
+    app.post("/categories", CategoryController.createCategory);
 }
