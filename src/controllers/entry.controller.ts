f2f74b91e94abdc89bf7683c9b35a02465e85126
Lucas <lucasapessoa@outlook.com>
2025-03-04 23:10:40 -0300

    add static entryRepository instance to EntryController

--- src/controllers/entry.controller.ts
+++ src/controllers/entry.controller.ts
@@ -2,4 +2,5 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { EntryRepository } from "../repositories/entry.repository";
 
 export class EntryController {
+    private static entryRepository = new EntryRepository();
 }
