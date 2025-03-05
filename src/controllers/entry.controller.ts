e91960fbf55525dfef892db73bb4c6b0f57a1871
Lucas <lucasapessoa@outlook.com>
2025-03-04 23:11:09 -0300

    add getActiveCategories method to EntryController

--- src/controllers/entry.controller.ts
+++ src/controllers/entry.controller.ts
@@ -3,4 +3,8 @@ import { EntryRepository } from "../repositories/entry.repository";
 
 export class EntryController {
     private static entryRepository = new EntryRepository();
+
+    static async getActiveCategories(req: FastifyRequest, reply: FastifyReply) {
+        return await EntryController.entryRepository.findActiveEntries();
+    }
 }
