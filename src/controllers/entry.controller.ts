0846e5d846c5be80a4422359c0d3081c59a988f6
Lucas <lucasapessoa@outlook.com>
2025-03-04 23:11:19 -0300

    add createEntry method to EntryController for handling entry creation

--- src/controllers/entry.controller.ts
+++ src/controllers/entry.controller.ts
@@ -7,4 +7,22 @@ export class EntryController {
     static async getActiveCategories(req: FastifyRequest, reply: FastifyReply) {
         return await EntryController.entryRepository.findActiveEntries();
     }
+    static async createEntry(req: FastifyRequest, reply: FastifyReply) {
+        const { userId, categoryId, value, description } = req.body as {
+            userId: number;
+            categoryId: number;
+            value: number;
+            description: string;
+        };
+
+        if (!userId || !categoryId || !value || !description) {
+            return reply.code(400).send({ message: "All fields are required" });
+        }
+        return EntryController.entryRepository.createEntry(
+            userId,
+            categoryId,
+            value,
+            description
+        );
+    }
 }
