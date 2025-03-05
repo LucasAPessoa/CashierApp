bb5b3e927bd928bffaf07f91c5148bd72f15e715
Lucas <lucasapessoa@outlook.com>
2025-03-04 23:11:54 -0300

    add EntryRoutes registration to server

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import fastify from "fastify";
 
 import { UserRoutes } from "./routes/user.routes";
 import { CategoryRoutes } from "./routes/category.routes";
+import { EntryRoutes } from "./routes/entry.routes";
 
 const app = fastify();
 
 app.register(CategoryRoutes);
 app.register(UserRoutes);
+app.register(EntryRoutes);
 
 app.listen({ port: 3000, host: "localhost" }, () => {
     console.log("Server is running on http://localhost:3000");
