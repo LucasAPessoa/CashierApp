13ef89c2061ec2060a38ac1580f3b3a643964c7c
Lucas <lucasapessoa@outlook.com>
2025-03-07 19:21:18 -0300

    refactor EntryController to use consistent parameter naming in getActiveEntries and createEntry methods

--- src/controllers/entry.controller.ts
+++ src/controllers/entry.controller.ts
@@ -4,12 +4,15 @@ import { EntryRepository } from "../repositories/entry.repository";
 export class EntryController {
     private static entryRepository = new EntryRepository();
 
-    static async getActiveEntries(req: FastifyRequest, reply: FastifyReply) {
+    static async getActiveEntries(
+        request: FastifyRequest,
+        reply: FastifyReply
+    ) {
         return await EntryController.entryRepository.findActiveEntries();
     }
 
-    static async createEntry(req: FastifyRequest, reply: FastifyReply) {
-        const { userId, categoryId, value, description } = req.body as {
+    static async createEntry(request: FastifyRequest, reply: FastifyReply) {
+        const { userId, categoryId, value, description } = request.body as {
             userId: number;
             categoryId: number;
             value: number;
