44349ace3903d6fc9b844586ac92ad5d1fe335d6
Lucas <lucasapessoa@outlook.com>
2025-03-07 19:23:39 -0300

    add updateEntry route in EntryRoutes

--- src/routes/entry.routes.ts
+++ src/routes/entry.routes.ts
@@ -4,4 +4,5 @@ import { EntryController } from "../controllers/entry.controller";
 export async function EntryRoutes(app: FastifyInstance) {
     app.post("/createEntry", EntryController.createEntry);
     app.patch("/deleteEntry/:id", EntryController.deleteEntry);
+    app.patch("/updateEntry/:id", EntryController.updateEntry);
 }
