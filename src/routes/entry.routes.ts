6c1c29c1eaa3676af3b607373e42b016e2227e8a
Lucas <lucasapessoa@outlook.com>
2025-03-07 19:23:47 -0300

    add getActiveEntries route in EntryRoutes

--- src/routes/entry.routes.ts
+++ src/routes/entry.routes.ts
@@ -3,6 +3,7 @@ import { EntryController } from "../controllers/entry.controller";
 
 export async function EntryRoutes(app: FastifyInstance) {
     app.post("/createEntry", EntryController.createEntry);
+    app.get("/activeEntries", EntryController.getActiveEntries);
     app.patch("/deleteEntry/:id", EntryController.deleteEntry);
     app.patch("/updateEntry/:id", EntryController.updateEntry);
 }
