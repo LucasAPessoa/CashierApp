c75b4c3fc6f6af5b3a3148d8db3794998ce869e7
Lucas <lucasapessoa@outlook.com>
2025-03-08 19:48:15 -0300

    refactor EntryRepository methods to include descriptive comments for clarity

--- src/repositories/entry.repository.ts
+++ src/repositories/entry.repository.ts
@@ -2,6 +2,7 @@ import { Prisma, Entries } from "@prisma/client";
 import { prisma } from "../lib/prisma";
 
 export class EntryRepository {
+    // Retorna uma entrada pelo id
     async findActiveEntriesById(id: number): Promise<Entries | null> {
         return await prisma.entries.findUnique({
             where: {
@@ -11,6 +12,7 @@ export class EntryRepository {
         });
     }
 
+    // Retorna todas as entradas pela categoria
     async findActiveEntriesByCategory(id: number): Promise<Entries[]> {
         return await prisma.entries.findMany({
             where: {
@@ -20,16 +22,19 @@ export class EntryRepository {
         });
     }
 
+    // Retorna todas as entradas ativas
     async findActiveEntries(): Promise<Entries[]> {
         return await prisma.entries.findMany({});
     }
 
+    // Cria uma nova entrada
     async createEntry(data: Prisma.EntriesCreateInput): Promise<Entries> {
         return await prisma.entries.create({
             data,
         });
     }
 
+    // Deleta uma entrada alternando o campo deletedAt para a data atual
     async deleteEntry(id: number): Promise<Entries> {
         return prisma.entries.update({
             where: {
@@ -41,6 +46,7 @@ export class EntryRepository {
         });
     }
 
+    // Altera as informações de uma entrada
     async updateEntry(
         id: number,
         data: Prisma.EntriesUpdateInput
