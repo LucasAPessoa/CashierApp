501ffd85d6fcc48ce187dcc690329e15570c3504
Lucas <lucasapessoa@outlook.com>
2025-03-15 22:20:53 -0300

    Add LoginPage component

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+function LoginPage(): ReactElement {
+    return (
+        <div>
+            <h1>LoginPage</h1>
+        </div>
+    );
+}
+
+export default LoginPage;
