2f1bfbe63d0ec2a083a0396e6af46167ff34ed61
Lucas <lucasapessoa@outlook.com>
2025-03-17 23:21:52 -0300

    Add TextInput component

---
+++ frontend/src/components/inputs/TextInput.tsx
@@ -0,0 +1,11 @@
+function TextInput() {
+    return (
+        <input
+            type="text"
+            required
+            className="rounded-xl border-2 border-black"
+        />
+    );
+}
+
+export default TextInput;
