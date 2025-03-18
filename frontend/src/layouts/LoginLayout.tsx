cd85d0288fc43e0334b5e8ff17eb19af345139aa
Lucas <lucasapessoa@outlook.com>
2025-03-17 23:24:56 -0300

    Enhance LoginLayout with styling for positioning, background and text alignment

--- frontend/src/layouts/LoginLayout.tsx
+++ frontend/src/layouts/LoginLayout.tsx
@@ -1,7 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { ReactElement } from "react";
 function LoginLayout(): ReactElement {
-    return <Outlet />;
+    return (
+        <div className="bg-bgPrimary text-textPrimary flex min-h-screen items-center justify-center overflow-hidden">
+            <Outlet />;
+        </div>
+    );
 }
 
 export default LoginLayout;
