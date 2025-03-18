0ecb6752c961c714829939af7d9309abeccc9900
Lucas <lucasapessoa@outlook.com>
2025-03-17 23:22:01 -0300

    Add LoginContainer component to encapsulate LoginForm

---
+++ frontend/src/components/layout/LoginContainer.tsx
@@ -0,0 +1,8 @@
+import LoginForm from "../forms/LoginForm";
+
+function LoginContainer() {
+    return <div className="h-max w-1/2 border-2 border-amber-950">
+        <LoginForm />
+    </div> // prettier-ignore
+}
+export default LoginContainer;
