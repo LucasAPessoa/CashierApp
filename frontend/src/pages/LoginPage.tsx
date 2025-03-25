f2c537a244903cf919afc65f39f4c7e70e8e1ab2
Lucas <lucasapessoa@outlook.com>
2025-03-25 20:13:10 -0300

    Remove ThemeToggler component from LoginPage for a cleaner layout

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,12 +1,10 @@
 import { ReactElement } from "react";
 import LoginContainer from "../components/layout/LoginContainer";
-import ThemeToggler from "../components/ui/ThemeToggler";
 
 function LoginPage(): ReactElement {
     return (
         <div className="bg-bgPrimary flex w-screen flex-col items-center justify-center">
             <LoginContainer />
-            <ThemeToggler />
         </div>
     );
 }
