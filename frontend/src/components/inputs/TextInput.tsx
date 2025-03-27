7cea08876d02c312ba4a4626677a8526a3401385
Lucas <lucasapessoa@outlook.com>
2025-03-27 16:08:22 -0300

    Update TextInput component styling for improved width and padding

--- frontend/src/components/inputs/TextInput.tsx
+++ frontend/src/components/inputs/TextInput.tsx
@@ -3,7 +3,7 @@ function TextInput() {
         <input
             type="text"
             required
-            className="border-textPrimary text-cs bordetext-fuchsia-200 h-10 rounded-full border-2 p-3"
+            className="border-textPrimary text-cs bordetext-fuchsia-200 h-10 w-2xs rounded-full border-2 px-3 py-1"
         />
     );
 }
