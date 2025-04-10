38fdf549c9d726f54e7999d048cbe38bf83c10ad
Lucas <lucasapessoa@outlook.com>
2025-04-10 11:58:15 -0300

    Refactor routing in App component to simplify structure and remove unnecessary layouts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LoginLayout />}>
-                    <Route path="/" element={<LoginPage />} />
-                </Route>
-                <Route path="/home" element={<MainLayout />}>
-                    <Route path="/home" element={<HomePage />} />
-                </Route>
+                <Route path="/" element={<LoginPage />}></Route>
+                <Route path="/home" element={<HomePage />}></Route>
             </Routes>
         </BrowserRouter>
     );
