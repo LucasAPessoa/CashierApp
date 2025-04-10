7ca2ae75ed86fd13c85aa1d409eaca9a9d8b95a1
Lucas <lucasapessoa@outlook.com>
2025-04-10 00:43:03 -0300

    Clean up frontend project structure to restart from scratch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import MainLayout from "./layouts/MainLayout";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import LoginLayout from "./layouts/LoginLayout";
 
 function App() {
     return (
--- frontend/src/components/layout/LoginContainer.tsx
+++
@@ -1,8 +0,0 @@
-import LoginForm from "../forms/LoginForm";
-
-function LoginContainer() {
-    return <div className="h-80 w-1/3 rounded-3xl shadow-xl ">
-        <LoginForm />
-    </div> // prettier-ignore
-}
-export default LoginContainer;
